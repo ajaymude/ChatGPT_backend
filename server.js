546bce49ce45b85969666b068e61689f852ae049
ajaymude <mudeajay@gmail.com>
2023-02-25 16:19:16 +0530

    add first tim 1vve

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import * as dotenv from "dotenv";
 import { Configuration, OpenAIApi } from "openai";
 
-const  port = process.env.PORT ||
+const  port = 'https://chatgpt-mern.onrender.com'
 
 
 dotenv.config();
